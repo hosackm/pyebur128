7c1d5ed43af882c66e8d070b8cc3e11ce429bfa0
Matt Hosack <hosack.matt@gmail.com>
2025-08-10 16:35:00 +0200

    bumping version in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from skbuild import setup
 
 setup(
     name="pyebur128",
-    version="0.1.0",
+    version="0.1.1",
     packages=["pyebur128"],
     package_dir={"": "src"},
     cmake_install_dir="src/pyebur128",
