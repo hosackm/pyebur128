4bfb0e36e206253450c06e5320d23f67c35ce7f7
Matt Hosack <hosack.matt@gmail.com>
2025-08-18 09:31:30 +0200

    feat: using context managed soundfile internally

--- src/pyebur128/meter.py
+++ src/pyebur128/meter.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from ._ffi import build_ffi_and_lib
 import soundfile as sf
 
@@ -8,32 +10,31 @@ class Meter:
     def __init__(self):
         self.ffi, self.lib = build_ffi_and_lib()
 
-    def measure(self, filepath: str, blocksize: int = 4096) -> float:
-        """Compute integrated loudness (LUFS) of the given audio file using streaming."""
-        info = sf.info(filepath)
-        samplerate = info.samplerate
-        channels = info.channels
+    def measure(self, filepath: Path | str, blocksize: int = 4096) -> float:
+        """Compute integrated loudness (LUFS) of the given audio file."""
+        with sf.SoundFile(filepath, "r") as f:
+            # Mode: EBUR128_MODE_I | EBUR128_MODE_LRA (0x1 | 0x4)
+            mode = 0x1 | 0x1 << 2
+            st = self.lib.ebur128_init(f.channels, f.samplerate, mode)
+            if not st:
+                raise RuntimeError("Failed to initialize ebur128_state")
 
-        # Mode: EBUR128_MODE_I | EBUR128_MODE_LRA (0x1 | 0x4)
-        mode = 0x1 | 0x1 << 2
-        st = self.lib.ebur128_init(channels, samplerate, mode)
-        if not st:
-            raise RuntimeError("Failed to initialize ebur128_state")
+            try:
+                while True:
+                    block = f.read(blocksize, always_2d=True)
+                    if len(block) == 0:
+                        break
 
-        try:
-            for block in sf.blocks(
-                filepath, blocksize=blocksize, dtype="float32", always_2d=True
-            ):
-                c_block = self.ffi.cast("float *", block.ctypes.data)
-                res = self.lib.ebur128_add_frames_float(st, c_block, block.shape[0])
+                    c_block = self.ffi.cast("float *", block.ctypes.data)
+                    res = self.lib.ebur128_add_frames_float(st, c_block, block.shape[0])
+                    if res != 0:
+                        raise RuntimeError("Failed to add frames")
+
+                out_loudness = self.ffi.new("double*")
+                res = self.lib.ebur128_loudness_global(st, out_loudness)
                 if res != 0:
-                    raise RuntimeError("Failed to add frames")
+                    raise RuntimeError("Failed to compute loudness")
 
-            out_loudness = self.ffi.new("double*")
-            res = self.lib.ebur128_loudness_global(st, out_loudness)
-            if res != 0:
-                raise RuntimeError("Failed to compute loudness")
-
-            return out_loudness[0]
-        finally:
-            self.lib.ebur128_destroy(self.ffi.new("ebur128_state**", st))
+                return out_loudness[0]
+            finally:
+                self.lib.ebur128_destroy(self.ffi.new("ebur128_state**", st))
